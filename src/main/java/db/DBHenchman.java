a59b3e8ef243ce7768dd7092469c53c697a91e9d
Can Toraman <cantoraman@gmail.com>
2018-07-13 18:04:02 +0100

    works on henchman quest fetching

--- src/main/java/db/DBHenchman.java
+++ src/main/java/db/DBHenchman.java
@@ -2,13 +2,45 @@ package db;
 
 import models.Henchman;
 import models.Hero;
+import models.Quest;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
 
 public class DBHenchman {
-//    public static void addHenchmanToHero(Henchman hm, Hero hero) {
-//        hm.setHero(hero);
+
+
+    private static Session session;
+    private static Transaction transaction;
+
+//    public static List<Quest> getQuestsFromHenchman(Henchman hm){
+//        List<Quest> quests = null;
 //
-////        hero.addHenchman(hm);
-////
-////        DBHelper.update(hero);
+//        session = HibernateUtil.getSessionFactory().openSession();
+//
+//        try{
+//            Criteria cr = session.createCriteria(Quest.class);
+//            cr.createAlias("heroes", "hero");
+//            cr.add(Restrictions.eq("hero.id", hm.getHero().getId()));
+//            quests = cr.list();
+//        }
+//        catch(HibernateException e){
+//            e.printStackTrace();
+//        }finally {
+//            session.close();
+//        }
+//        return quests;
 //    }
+
+
+
+
+
+
+
+
 }
