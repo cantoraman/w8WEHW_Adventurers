65ccf0c6124d90840b3a961d2dcf93fa42f6a689
Can Toraman <cantoraman@gmail.com>
2018-07-13 12:40:59 +0100

    sets up quest class

--- src/main/java/models/Quest.java
+++ src/main/java/models/Quest.java
@@ -1,9 +1,45 @@
 package models;
 
-public class Quest {
+import java.util.List;
 
+public class Quest {
+    private String name;
     private int awardTreasure;
     private int awardExperience;
+    private List<Hero> heroes;
 
 
+    public Quest(String name, int awardTreasure, int awardExperience) {
+        this.name = name;
+        this.awardTreasure = awardTreasure;
+        this.awardExperience = awardExperience;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAwardExperience() {
+        return awardExperience;
+    }
+    public void setAwardExperience(int awardExperience) {
+        this.awardExperience = awardExperience;
+    }
+
+    public int getAwardTreasure() {
+        return awardTreasure;
+    }
+    public void setAwardTreasure(int awardTreasure) {
+        this.awardTreasure = awardTreasure;
+    }
+
+    public List<Hero> getHeroes() {
+        return heroes;
+    }
+    public void setHeroes(List<Hero> heroes) {
+        this.heroes = heroes;
+    }
 }
---
+++ src/test/java/CompaniesTest.java
@@ -0,0 +1,10 @@
+import org.junit.Before;
+
+public class CompaniesTest {
+
+
+    @Before
+
+
+
+}
